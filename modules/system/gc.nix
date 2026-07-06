e4bfd68452e3f58d21808b04124fb1c2f0e2e504
quangnguyen0101 <quangdangtruongnguyen@gmail.com>
2026-07-06 10:29:54 +0700

    Garbage collection for NixOS

--- modules/system/gc.nix
+++ modules/system/gc.nix
@@ -4,7 +4,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 14d";
   };
 
   # Bonus: tự tối ưu hóa store, giảm dung lượng trùng lặp
