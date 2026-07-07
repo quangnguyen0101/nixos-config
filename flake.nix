7225b08979d18300b9738ad90b67f922215bd637
quangnguyen0101 <quangdangtruongnguyen@gmail.com>
2026-07-07 17:57:17 +0700

    Add lanzaboote for secure boot

--- flake.nix
+++ flake.nix
@@ -1,34 +1,63 @@
 {
-	description = "NixOS configuration";
+  description = "NixOS configuration";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-		home-manager.url = "github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		
-		caelestia-shell = {
-			url = "github:caelestia-dots/shell";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-	outputs =
-	inputs@{ self, nixpkgs, home-manager, ... }:
-	{
-		nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem 
-		{
-			system = "x86_64-linux";
-			modules = 
-			[
-				./hosts/nixos-btw/configuration.nix
-				home-manager.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.extraSpecialArgs = { inherit inputs; };
-						home-manager.users.sh4d0wph4nt0m = ./home/sh4d0wph4nt0m/home.nix;
-					}
-        		];
-		};
-	};
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v1.1.0";
+
+      # Optional but recommended to limit the size of your system closure.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nixos-btw/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.sh4d0wph4nt0m = ./home/sh4d0wph4nt0m/home.nix;
+          }
+
+          lanzaboote.nixosModules.lanzaboote
+          ({ pkgs, lib, ... }: {
+            environment.systemPackages = [
+              # For debugging and troubleshooting Secure Boot.
+              pkgs.sbctl
+            ];
+
+            # Lanzaboote currently replaces the systemd-boot module.
+            # This setting is usually set to true in configuration.nix
+            # generated at installation time. So we force it to false
+            # for now.
+            boot.loader.systemd-boot.enable = lib.mkForce false;
+
+            boot.lanzaboote = {
+              enable = true;
+              pkiBundle = "/var/lib/sbctl";
+            };
+          })
+        ];
+      };
+    };
 }
