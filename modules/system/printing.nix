1923588f4c47eec93b867e95b2818b65674a46d5
quangnguyen0101 <quangdangtruongnguyen@gmail.com>
2026-07-30 20:38:25 +0700

    nothing

--- modules/system/printing.nix
+++ modules/system/printing.nix
@@ -1,8 +1,23 @@
 { pkgs, ... }:
 
 {
+  # ===== In ấn =====
   services.printing = {
     enable = true;
-    drivers = [ pkgs.epson-escpr ];
+    drivers = [
+      pkgs.epson-escpr
+    ];
   };
+
+  # # ===== Scan =====
+  # hardware.sane = {
+  #   enable = true;
+  #   extraBackends = [ pkgs.epkowa ];
+  # };
+  #
+  # environment.systemPackages = with pkgs; [
+  #   sane-frontends # cung cấp lệnh `scanimage` (CLI) + `xscanimage`
+  #   simple-scan # GUI đơn giản, dễ dùng hàng ngày
+  #   xsane # GUI nâng cao hơn, nhiều tùy chỉnh (crop, DPI, mode màu...)
+
 }
