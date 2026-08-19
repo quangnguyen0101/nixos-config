6c89bfe530c52ee9f61378dad96a52353423c8c1
quangnguyen0101 <quangdangtruongnguyen@gmail.com>
2026-08-19 21:40:47 +0700

    add ollama model to opencode

--- modules/home/userPackages.nix
+++ modules/home/userPackages.nix
@@ -45,7 +45,29 @@
   xdg.configFile."opencode/opencode.jsonc".text = ''
     {
       "$schema": "https://opencode.ai/config.json",
-      "disabled_providers": ["ollama"]
+      "provider": {
+        "ollama": {
+          "npm": "@ai-sdk/openai-compatible",
+          "name": "Ollama",
+          "options": {
+            "baseURL": "http://localhost:11434/v1"
+          },
+          "models": {
+            "minimax-m2.7:cloud": {
+              "name": "MiniMax M2.7"
+            },
+            "nemotron-3-super:cloud": {
+              "name": "Nemotron 3 Super"
+            },
+            "gpt-oss:120b-cloud": {
+              "name": "GPT-OSS 120B"
+            },
+            "gemma4:31b-cloud": {
+              "name": "Gemma 4 31B"
+            }
+          }
+        }
+      }
     }
   '';
 }
