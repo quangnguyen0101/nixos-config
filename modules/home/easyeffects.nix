b49c5596f5d5eee497c70cd2a7e9b3b6695aa37e
quangnguyen0101 <quangdangtruongnguyen@gmail.com>
2026-08-19 23:08:33 +0700

    add easyeffects

--- modules/home/easyeffects.nix
+++ modules/home/easyeffects.nix
@@ -1,27 +1,41 @@
 { pkgs, lib, ... }:
 
 let
-  presets = pkgs.fetchFromGitHub {
+  jackhack96 = pkgs.fetchFromGitHub {
     owner = "JackHack96";
     repo = "EasyEffects-Presets";
     rev = "dd966e41ad9e44d4b11e19047f526ba718bbbe57";
     hash = "sha256-JpQVWuEokBRu01xkGA22dPeV5Jo8Xzvfrg5oQ8RtIrI=";
   };
 
-  jsonFiles = lib.filterAttrs (name: _: lib.hasSuffix ".json" name) (builtins.readDir presets);
+  digitalone1 = pkgs.fetchFromGitHub {
+    owner = "Digitalone1";
+    repo = "EasyEffects-Presets";
+    rev = "6fc0630f3d18f5668b11ebe4846179914b0bd24e";
+    hash = "sha256-MQ72Tg4YAlzsRrQtVOUxaQKNmbxUjaci+FAr2MvjrEQ=";
+  };
+
+  jsonFiles = name: src:
+    lib.filterAttrs (n: _: lib.hasSuffix ".json" n) (builtins.readDir src);
+
+  allJsonFiles = (jsonFiles "jackhack96" jackhack96) // (jsonFiles "digitalone1" digitalone1);
+
+  mkPresetLink = src: name: _:
+    lib.nameValuePair "easyeffects/output/${name}" {
+      source = "${src}/${name}";
+    };
+
+  jackhack96Links = lib.mapAttrs' (mkPresetLink jackhack96) (jsonFiles "jackhack96" jackhack96);
+  digitalone1Links = lib.mapAttrs' (mkPresetLink digitalone1) (jsonFiles "digitalone1" digitalone1);
 in
 {
   services.easyeffects = {
     enable = true;
   };
 
-  xdg.configFile = lib.mapAttrs' (name: _:
-    lib.nameValuePair "easyeffects/output/${name}" {
-      source = "${presets}/${name}";
-    }
-  ) jsonFiles // {
+  xdg.configFile = jackhack96Links // digitalone1Links // {
     "easyeffects/irs" = {
-      source = "${presets}/irs";
+      source = "${jackhack96}/irs";
       recursive = true;
     };
   };
